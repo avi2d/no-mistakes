737e9f7c9210080bad3a9b6b4714ee5743047a8f
avi2d <avi2dg@gmail.com>
2026-09-23 21:39:14 +0300

    throwaway: fix formatting (the green half of the proof)

--- throwaway_ci_proof.go
+++ throwaway_ci_proof.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func throwawayCiProof() {
-    fmt.Println("this file is intentionally misformatted to prove fork CI rejects it")
+	fmt.Println("this file is intentionally misformatted to prove fork CI rejects it")
 }
